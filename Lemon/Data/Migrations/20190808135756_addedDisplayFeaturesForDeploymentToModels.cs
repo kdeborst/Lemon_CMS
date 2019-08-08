41d9076058953bb1783a6a3f58fe9d8a1135d582
Kevin de Borst <kevin.de.borst@gmail.com>
2019-08-08 16:00:04 +0200

    Last Migration & Deployment

---
+++ Lemon/Data/Migrations/20190808135756_addedDisplayFeaturesForDeploymentToModels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lemon.Data.Migrations
+{
+    public partial class addedDisplayFeaturesForDeploymentToModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
